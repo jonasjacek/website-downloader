346d50e8fa79e233ac18708e6dd16ccb28a5df12
Jonas Jared Jacek <mail@jonas.me>
2020-03-12 20:28:36 +0100

    script draft

--- website-downloader.sh
+++ website-downloader.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 # Small Bash script to iterate over a list of URLs and download them incl. assets.
+
+wget -r -x -k --no-cache -w 10 -e robots=off --show-progress --progress=bar -i website-downloader_urls.txt
